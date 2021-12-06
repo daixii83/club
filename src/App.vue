dd23f15559723ea91fcf7d1fa69d4d76ffee17af
daixii83 <daixii83@gmail.com>
2021-12-06 13:35:22 +0800

    20211206
    WEBPACK

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+#app
+  Top
+  #middle
+    #left
+      Menu
+      Update
+    #right
+      Marquee
+      Content
+  Footer
+</template>
+
+<script>
+// 引用自己的元件
+import Top from './components/Top.vue'
+import Menu from './components/Menu'
+import Update from './components/Update.vue'
+import Marquee from './components/Marquee.vue'
+import Content from './components/Content'
+import Footer from './components/Footer.vue'
+export default {
+  name: 'App',
+  // 引用後註冊元件
+  components: {
+    Top,
+    Menu,
+    Update,
+    Marquee,
+    Content,
+    Footer
+  }
+}
+</script>
